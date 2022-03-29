12ced35b0233d157a1cac40c8e7e3e1a8e4b0610
r_suzuki <r_suzuki@fantec.jp>
2022-03-29 17:49:53 +0900

    docs

--- php/main.php
+++ php/main.php
@@ -1,5 +1,8 @@
 <?php
+
     $text = file_get_contents('../data/kagu.txt');
-    $text = json_decode($text,true);
+    $text = json_decode(json_decode($text,true),true);
     var_dump($text);
+
+
 ?>
