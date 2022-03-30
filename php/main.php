dfc1ca89ba5222e1c53b12128cd2883f6bcdfae8
r_suzuki <r_suzuki@fantec.jp>
2022-03-30 12:05:19 +0900

    insert

--- php/main.php
+++ php/main.php
@@ -1,68 +1,104 @@
 <?php
+    require_once './lib.php';
+
+    $ins_str = sprintf("insert into items (type,sub_type,name,img,color) values ");
 
     $text = file_get_contents('../data/kagu.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(0,0,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/komono.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(0,1,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/kabekake.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(0,2,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/tenjo.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(0,3,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/ryori.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(0,4,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/mairukagu.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(0,5,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/tops.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,0,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/bottoms.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,1,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/onepiece.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,2,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/kaburimono.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,3,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/accesally.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,4,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/kutsusita.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,5,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/kutsu.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,6,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/bag.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,7,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/kasa.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,8,'%s','%s','%s'),",$val[0],$val[1],$val[2]);
+    }
 
     $text = file_get_contents('../data/fassionsonota.txt');
-    $text = json_decode(json_decode($text,true),true);
-    echo gettype($text);
+    $arr = json_decode(json_decode($text,true),true);
+    foreach($arr as $val){
+        $ins_str .= sprintf("(1,9,'%s','%s','%s')",$val[0],$val[1],$val[2]);
+    }
 
+    $ins_res = mysqli_query($conn,$ins_str);
 
 ?>
