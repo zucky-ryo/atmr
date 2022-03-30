c79ddf114273ece8f230ac65a8a87a24c9bb071b
r_suzuki <r_suzuki@fantec.jp>
2022-03-30 12:50:48 +0900

    kagudake

--- php/main.php
+++ php/main.php
@@ -39,66 +39,67 @@
         $ins_str .= sprintf("(0,5,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
     }
 
-    $text = file_get_contents('../data/tops.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,0,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/tops.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,0,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
-    $text = file_get_contents('../data/bottoms.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,1,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/bottoms.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,1,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
-    $text = file_get_contents('../data/onepiece.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,2,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/onepiece.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,2,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
-    $text = file_get_contents('../data/kaburimono.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,3,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/kaburimono.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,3,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
-    $text = file_get_contents('../data/accesally.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,4,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/accesally.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,4,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
-    $text = file_get_contents('../data/kutsusita.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,5,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/kutsusita.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,5,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
-    $text = file_get_contents('../data/kutsu.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,6,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/kutsu.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,6,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
-    $text = file_get_contents('../data/bag.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,7,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/bag.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,7,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
-    $text = file_get_contents('../data/kasa.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,8,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/kasa.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,8,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
-    $text = file_get_contents('../data/fassionsonota.txt');
-    $arr = json_decode(json_decode($text,true),true);
-    foreach($arr as $key => $val){
-        $ins_str .= sprintf("(1,9,'%s','%s','%s');",$val['name'],$val['img'],$val['type']);
-    }
+    // $text = file_get_contents('../data/fassionsonota.txt');
+    // $arr = json_decode(json_decode($text,true),true);
+    // foreach($arr as $key => $val){
+    //     $ins_str .= sprintf("(1,9,'%s','%s','%s'),",$val['name'],$val['img'],$val['type']);
+    // }
 
+    $ins_str = substr($ins_str, 0, -1);
     $ins_res = mysqli_query($conn,$ins_str);
 
     if($ins_res){
