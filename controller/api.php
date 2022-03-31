40dd3c478f30117d7b63e8d8fdcd874ec6880846
r_suzuki <r_suzuki@fantec.jp>
2022-03-31 13:26:29 +0900

    start

--- controller/api.php
+++ controller/api.php
@@ -10,9 +10,9 @@
         $menu_str = "type_".$type."_arr";
         
         $html = "";
-        $html .= sprintf("<div class='d-flex scroll_x' style='height: 30px;'>");
+        $html .= sprintf("<div class='d-flex scroll_x mb-2' style='height: 30px;'>");
         foreach($$menu_str as $menu){
-            $html .= sprintf("<div class='mx-1 px-2 text-center border border-success rounded-pill'>%s</div>",$menu);
+            $html .= sprintf("<button class='mx-1 px-2 btn btn-outline-success text-center pointer'>%s</button>",$menu);
         }
         $html .= sprintf("</div>");
         $html .= sprintf("<div class=''>");
