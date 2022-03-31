1718afc4f7716bcc00bb3b9d37ebf4f029252f8f
r_suzuki <r_suzuki@fantec.jp>
2022-03-31 16:24:46 +0900

    start

--- controller/api.php
+++ controller/api.php
@@ -30,7 +30,7 @@
             if($i % 6 == 0 || $i % 6 == 5){
                 $html .= sprintf("<div class='col-1'></div>");
             }else{
-                $html .= sprintf("<div class='col-xl-1 col-md-2 col-3 border border-white px-1'>");
+                $html .= sprintf("<div class='col-xl-1 col-2 border border-white px-1'>");
                 $html .= sprintf("<div class='py-1' style='font-size: 10px; font-weight: bold;'>%s</div>",$item['name']);
                 $html .= sprintf("<div class='d-flex justify-content-center'><img data-src='%s' class='lazyload' width='80%%' height='100%%'></div>",$item['img'],$item['img']);
                 $html .= sprintf("<div class='text-center mb-1' style='font-size: 10px;'>%s</div>",$item['color']);
