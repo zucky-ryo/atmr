876f4169360db9b11f33f200d0f3f16eaaf906de
r_suzuki <r_suzuki@fantec.jp>
2022-03-31 16:13:23 +0900

    start

--- controller/api.php
+++ controller/api.php
@@ -28,10 +28,10 @@
         $html .= sprintf("<div class='col-10'>");
         $html .= sprintf("<div class='row bg-light'>");
         foreach($items as $item){
-            $html .= sprintf("<div class='col-xl-1 col-md-2 col-3 border border-white px-2'>");
+            $html .= sprintf("<div class='col-xl-1 col-md-2 col-3 border border-white px-1'>");
             $html .= sprintf("<div class='py-1' style='font-size: 10px; font-weight: bold;'>%s</div>",$item['name']);
-            $html .= sprintf("<div><img data-src='%s' class='lazyload' width='100%%' height='100%%'></div>",$item['img'],$item['img']);
-            $html .= sprintf("<div class='text-center py-1' style='font-size: 10px;'>%s</div>",$item['color']);
+            $html .= sprintf("<div><img data-src='%s' class='lazyload' width='80%%' height='100%%'></div>",$item['img'],$item['img']);
+            $html .= sprintf("<div class='text-center mb-1' style='font-size: 10px;'>%s</div>",$item['color']);
             $html .= sprintf("</div>");
         }
         $html .= sprintf("</div>");
