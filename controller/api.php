f72da57e86c6eaa4cf406b755fcba4fea2179498
r_suzuki <r_suzuki@fantec.jp>
2022-04-01 17:50:18 +0900

    start

--- controller/api.php
+++ controller/api.php
@@ -16,35 +16,35 @@
         $title = $type_arr[$type];
         $s_type = $_GET['s_type'];
         $s_title = "全て";
-        // $items = getItem($type,$s_type);
+        $items = getItem($type,$s_type);
         $menu_str = "type_".$type."_arr";
         $btn = "btn-outline-success";
         if($s_type == ""){ $btn = "btn-success"; }
         
         $html = "";
-        $html .= sprintf("<div class='d-flex scroll_x mb-2 px-2'>");
-        $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,``)'>全て</button>",$btn,$type);
-        foreach($$menu_str as $key => $menu){
-            $btn = "btn-outline-success";
-            if($s_type == $key){ $btn = "btn-success"; $s_title = $$menu_str[$key]; }
-            $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,%d)'>%s</button>",$btn,$type,$key,$menu);
-        }
-        $html .= sprintf("</div>");
-        $html .= sprintf("<div class='px-1'>");
-        $html .= sprintf("<div class=''>");
-        $html .= sprintf("<div class=''>");
-        $html .= sprintf("<div class='d-flex flex-wrap bg-light'>");
-        foreach($items as $i => $item){
-            $html .= sprintf("<div class='border border-white px-1' style='width: 20%%;'>");
-            $html .= sprintf("<div class='py-1' style='font-size: 10px; font-weight: bold;'>%s</div>",$item['name']);
-            $html .= sprintf("<div class='d-flex justify-content-center'><img data-src='%s' class='lazyload' width='80%%' height='100%%'></div>",$item['img'],$item['img']);
-            $html .= sprintf("<div class='text-center mb-1' style='font-size: 10px;'>%s</div>",$item['color']);
-            $html .= sprintf("</div>");
-        }
-        $html .= sprintf("</div>");
-        $html .= sprintf("</div>");
-        $html .= sprintf("</div>");
-        $html .= sprintf("</div>");
+        // $html .= sprintf("<div class='d-flex scroll_x mb-2 px-2'>");
+        // $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,``)'>全て</button>",$btn,$type);
+        // foreach($$menu_str as $key => $menu){
+        //     $btn = "btn-outline-success";
+        //     if($s_type == $key){ $btn = "btn-success"; $s_title = $$menu_str[$key]; }
+        //     $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,%d)'>%s</button>",$btn,$type,$key,$menu);
+        // }
+        // $html .= sprintf("</div>");
+        // $html .= sprintf("<div class='px-1'>");
+        // $html .= sprintf("<div class=''>");
+        // $html .= sprintf("<div class=''>");
+        // $html .= sprintf("<div class='d-flex flex-wrap bg-light'>");
+        // foreach($items as $i => $item){
+        //     $html .= sprintf("<div class='border border-white px-1' style='width: 20%%;'>");
+        //     $html .= sprintf("<div class='py-1' style='font-size: 10px; font-weight: bold;'>%s</div>",$item['name']);
+        //     $html .= sprintf("<div class='d-flex justify-content-center'><img data-src='%s' class='lazyload' width='80%%' height='100%%'></div>",$item['img'],$item['img']);
+        //     $html .= sprintf("<div class='text-center mb-1' style='font-size: 10px;'>%s</div>",$item['color']);
+        //     $html .= sprintf("</div>");
+        // }
+        // $html .= sprintf("</div>");
+        // $html .= sprintf("</div>");
+        // $html .= sprintf("</div>");
+        // $html .= sprintf("</div>");
 
         $json = ['html' => $html, 'title' => $title, 'sub_title' => $s_title];
         $_SESSION['type'] = $_GET['type'];
