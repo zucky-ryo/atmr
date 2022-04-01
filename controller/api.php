153d36ef73f6f1c0a0129b83502aa16e8d2c83da
r_suzuki <r_suzuki@fantec.jp>
2022-04-01 18:05:38 +0900

    start

--- controller/api.php
+++ controller/api.php
@@ -30,7 +30,7 @@
             $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,%d)'>%s</button>",$btn,$type,$key,$menu);
         }
         $html .= sprintf("</div>");
-        $html .= sprintf("<div class='d-flex justify-content-center align-items-center scroll_x mb-2 px-2'>");
+        $html .= sprintf("<div class='d-flex scroll_x mb-2 px-2'>");
         $arr = ["A","あ","か","さ","た","な","は","ま","や","ら","わ"];
         for($i=0;$i<11;$i++){
             $html .= sprintf("<button type='button' class='mx-1 px-2 btn btn btn-outline-dark text-center'>%s</button>",$arr[$i]);
