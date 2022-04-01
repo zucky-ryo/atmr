a4b53e02ceccc8d10efcc47331f17132d71d084d
r_suzuki <r_suzuki@fantec.jp>
2022-04-01 17:51:02 +0900

    start

--- controller/api.php
+++ controller/api.php
@@ -22,13 +22,13 @@
         if($s_type == ""){ $btn = "btn-success"; }
         
         $html = "";
-        // $html .= sprintf("<div class='d-flex scroll_x mb-2 px-2'>");
-        // $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,``)'>全て</button>",$btn,$type);
-        // foreach($$menu_str as $key => $menu){
-        //     $btn = "btn-outline-success";
-        //     if($s_type == $key){ $btn = "btn-success"; $s_title = $$menu_str[$key]; }
-        //     $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,%d)'>%s</button>",$btn,$type,$key,$menu);
-        // }
+        $html .= sprintf("<div class='d-flex scroll_x mb-2 px-2'>");
+        $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,``)'>全て</button>",$btn,$type);
+        foreach($$menu_str as $key => $menu){
+            $btn = "btn-outline-success";
+            if($s_type == $key){ $btn = "btn-success"; $s_title = $$menu_str[$key]; }
+            $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,%d)'>%s</button>",$btn,$type,$key,$menu);
+        }
         // $html .= sprintf("</div>");
         // $html .= sprintf("<div class='px-1'>");
         // $html .= sprintf("<div class=''>");
