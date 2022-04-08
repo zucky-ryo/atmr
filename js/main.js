bbcdf07415d8e79b615c3164f1d7bede2aee71af
r_suzuki <r_suzuki@fantec.jp>
2022-04-08 13:12:27 +0900

    start

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ function main_scroll(sw){
         $(window).scrollTop($('#scroll').position().top);
     }
     if(sw == 1 && typeof $('#scroll_a').position() !== "undefined"){
-        $(window).scrollTop($('#scroll_a').position().top - 140);
+        $(window).scrollTop($('#scroll_a').position().top - 145);
     }
     if(sw == 2 && typeof $('#scroll_k').position() !== "undefined"){
         $(window).scrollTop($('#scroll_k').position().top);
