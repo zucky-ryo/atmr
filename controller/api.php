3e47ae7993f8ba70934536ff2e59f9ebb29fb508
r_suzuki <r_suzuki@fantec.jp>
2022-04-11 18:40:05 +0900

    start

--- controller/api.php
+++ controller/api.php
@@ -39,7 +39,7 @@
         if($s_type == ""){ $btn = "btn-success"; }
         
         $html = "";
-        $html .= sprintf("<div class='bg-white py-1' style='position: fixed;'>");
+        $html .= sprintf("<div class='bg-white py-1' style='position: fixed; z-index: 100;'>");
         $html .= sprintf("<div class='d-flex scroll_x mb-2 px-2'>");
         $html .= sprintf("<button type='button' class='mx-1 px-2 btn %s text-center' onclick='show_page(%d,``)'>全て</button>",$btn,$type);
         foreach($$menu_str as $key => $menu){
@@ -55,7 +55,7 @@
         }
         $html .= sprintf("</div>");
         $html .= sprintf("</div>");
-        $html .= sprintf("<div class='px-1' style='padding-top: 90px;'>");
+        $html .= sprintf("<div class='px-1' style='padding-top: 90px; z-index: 10;'>");
         $top_html = $html;
         $html = "";
         $html .= sprintf("<div class=''>");
