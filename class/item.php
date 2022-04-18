78435e9d33465f4d75bb0868a239fa57d018d5b7
r_suzuki <r_suzuki@fantec.jp>
2022-04-18 17:55:30 +0900

    start

--- class/item.php
+++ class/item.php
@@ -31,7 +31,7 @@
             if($s_type != ""){ $s_type = sprintf("and sub_type='%d'",$s_type); }
             $sql  = sprintf("select A.*,B.item_id as sw from items as A left join user_item_relations as B ");
             $sql .= sprintf("on A.id = B.item_id and (B.user_id = '%d' or B.guest_id = '%s') ",$user_id,$user_id);
-            $sql .= sprintf("where type='%d' %s order by replace(kana, 'ー', 'あ') ASC",$type,$s_type);
+            $sql .= sprintf("where type='%d' %s order by replace(kana, 'ー', 'あ') regexp '^[a-z]' then 1 else 2 end desc,kana ASC",$type,$s_type);
             $res = mysqli_query($this->conn,$sql);
             while($row = mysqli_fetch_assoc($res)){
                 $items[] = $row;
