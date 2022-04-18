ad5d8fd6d6bfcee96e62d472af58e42631e5ef0b
r_suzuki <r_suzuki@fantec.jp>
2022-04-18 18:31:05 +0900

    start

--- class/item.php
+++ class/item.php
@@ -31,7 +31,8 @@
             if($s_type != ""){ $s_type = sprintf("and sub_type='%d'",$s_type); }
             $sql  = sprintf("select A.*,B.item_id as sw from items as A left join user_item_relations as B ");
             $sql .= sprintf("on A.id = B.item_id and (B.user_id = '%d' or B.guest_id = '%s') ",$user_id,$user_id);
-            $sql .= sprintf("where type='%d' %s order by case when TRIM(kana) regexp '^[0-9]' then 1 when TRIM(kana) regexp '^[a-z]' then 2 else 3 end desc,replace(kana, 'ー', 'あ') ASC",$type,$s_type);
+            $sql .= sprintf("where type='%d' %s order by case when TRIM(kana) regexp '^[0-9]' then 1 when TRIM(kana) regexp '^[a-z]' then 2 else 3 end desc,",$type,$s_type);
+            $sql .= sprintf("replace(replace(kana, 'ー', 'あ'),'ヴ','ウ') ASC");
             $res = mysqli_query($this->conn,$sql);
             while($row = mysqli_fetch_assoc($res)){
                 $items[] = $row;
