7b4ddd8262e6561fee2a7bd5c4f7d237469ef253
r_suzuki <r_suzuki@fantec.jp>
2022-04-18 18:41:03 +0900

    start

--- controller/api.php
+++ controller/api.php
@@ -120,7 +120,7 @@
                 }else{
                     $before_name = $item['name'];
                 }
-                $html .= sprintf("<div class='py-1' style='font-size: 10px; font-weight: bold;'>%s</div>",$item['name']);
+                $html .= sprintf("<div class='py-1' style='font-size: 10px; font-weight: bold; height: 15px;'>%s</div>",$item['name']);
                 $html .= sprintf("<div class='d-flex justify-content-center'><img data-src='%s' class='lazyload' width='80%%' height='100%%'></div>",$item['img'],$item['img']);
                 $html .= sprintf("<div class='text-center mb-1' style='font-size: 10px;'>%s</div>",$item['color']);
                 $html .= sprintf("</div>");
